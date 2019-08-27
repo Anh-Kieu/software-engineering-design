2e4df202428586d7a8f2391e69cc5863bc282efd
aqk001 <aqk001@bucknell.edu>
2019-08-27 17:38:11 -0400

    Lab01, Ex 5, complete

---
+++ src/lab01/Hello.java
@@ -0,0 +1,24 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Fall 2019
+* Instructor: Prof. King
+* Section: 9 am
+*
+* Name: Anh Kieu
+* Date: 08/27/2019
+*
+* Lab / Assignment: lab01
+*
+* Description: Getting Started - Hello World program
+*
+* *****************************************/
+
+package lab01;
+
+public class Hello
+{
+  public static void main( String[] args ) {
+    System.out.println("Programming is not a spectator sport!");
+    System.exit(0);
+  }
+}
