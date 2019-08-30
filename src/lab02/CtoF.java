e294f3da0f7fd21f66abf0767e315bd356a8e38c
aqk001 <aqk001@bucknell.edu>
2019-08-29 22:49:39 -0400

    Lab02, Ex 4, complete

--- src/lab02/CtoF.java
+++ src/lab02/CtoF.java
@@ -21,20 +21,41 @@ import java.util.Scanner;
 
 public class CtoF {
     public static void main(String[] args) {
+        while (true) {
+          // Prompt user for a Celsius temp
+          System.out.printf("Enter a temperature in Celsius: ");
 
-        // Prompt user for a Celsius temp
-        System.out.printf("Enter a temperature in Celsius: ");
+          // Create the Scanner object, attached to console input
+          Scanner in = new Scanner(System.in);
 
-        // Create the Scanner object, attached to console input
-        Scanner in = new Scanner(System.in);
+          // Read and store the Celsius temp
+          int celTemp = in.nextInt();
 
-        // Read and store the Celsius temp
-        int celTemp = in.nextInt();
+          // Convert the temp to Fahrenheit
+          double fahTemp = ((double) celTemp * 9) / 5 + 32;
 
-        // Convert the temp to Fahrenheit
-        double fahTemp = ((double) celTemp * 9) / 5 + 32;
+          // Output the results with correct formatting, one significant digit
+          System.out.printf("%d C = %.1f F\n", celTemp, fahTemp);
 
-        // Output the results with correct formatting, one significant digit
-        System.out.printf("%d C = %.1f F\n", celTemp, fahTemp);
+          // Output the additional comment
+          if (fahTemp <= 32) {
+            System.out.println("Brrr... it is FREEZING out!");
+          } else if (32 < fahTemp && fahTemp <= 60) {
+            System.out.println("It's a bit cool out");
+          } else if (60 < fahTemp && fahTemp <= 85) {
+            System.out.println("It's comfortably warm");
+          } else {
+            System.out.println("It's HOT! I need A/C!");
+          }
+
+          // Prompt user for another try:
+          System.out.printf("Try again? (y|n): ");
+          String again = in.next();
+
+          // Handle break
+          if (again.equalsIgnoreCase("n"))
+            break;
+        }
+        System.out.println("Goodbye!");
     }
 }
