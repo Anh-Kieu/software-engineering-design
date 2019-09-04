0a19e39fd956abdd73e48d8e1dfde1d03617bd1c
aqk001 <aqk001@bucknell.edu>
2019-09-04 02:07:18 -0400

    NonVowel

---
+++ src/lab03/StringChallenge.java
@@ -0,0 +1,85 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Fall 2019
+* Instructor: Prof. King
+* Section: 9 am
+*
+* Name: Anh Kieu
+* Date: 09/02/2019
+*
+* Lab / Assignment: lab03
+*
+* Description: Git, Methods and String class
+*
+* *****************************************/
+package lab03;
+
+/**
+ * The StringChallenge class is used to evaluate several different string
+ * exercises from lab03.
+ *
+ * @author brk009
+ * @version 0.1
+ */
+public class StringChallenge {
+
+    /**
+     * Count the number of non-vowels (consonants) out of all letters in a string.
+     *
+     * @param s The string to test
+     * @return The number of vowels in <code>s</code>
+     */
+    public static int countNonVowels(String s) {
+        int notNonVowel = 0;
+        for (int i=0; i<s.length(); i++) {
+          if (s.charAt(i) == 'a' || s.charAt(i) == 'u' || s.charAt(i) == 'e' || s.charAt(i) == 'o' ||  s.charAt(i) == 'i' || !Character.isLetter(s.charAt(i))) notNonVowel ++;
+        }
+        return s.length() - notNonVowel;
+    }
+
+    /**
+     * Count the number of non-vowels (consonants) out of all letters in a string.
+     * However, this solution uses recursion.
+     *
+     * @param s The string to test
+     * @return The number of vowels in <code>s</code>
+     */
+    public static int countNonVowelsRec(String s) {
+        if (s.length() == 0) return 0;
+        else {
+          char first_char = s.charAt(0);
+          if (!(first_char == 'a' || first_char == 'u' || first_char == 'e' || first_char == 'o' ||  first_char == 'i' || !Character.isLetter(first_char))) return 1 + countNonVowelsRec(s.substring(1));
+          return countNonVowels(s.substring(1));
+        }
+    }
+
+    /**
+     * Main program to test out each String processing method
+     */
+    public static void main(String[] args) {
+
+        String sTest = "Mississippi River";
+        System.out.printf("countNonVowels(\"%s\") = %d\n", sTest, countNonVowels(sTest));
+        System.out.printf("countNonVowelsRec(\"%s\") = %d\n", sTest, countNonVowelsRec(sTest));
+//
+//        System.out.printf("isPalindrome(\"%s\") = %b\n", sTest, isPalindrome(sTest));
+//        sTest = "amanapanama";
+//        System.out.printf("isPalindrome(\"%s\") = %b\n", sTest, isPalindrome(sTest));
+//
+//        sTest = "Testing";
+//        System.out.printf("swapLastAndFirstLetter(\"%s\") = \"%s\"\n", sTest, swapLastAndFirstLetter(sTest));
+//        sTest = "Testing123";
+//        System.out.printf("swapLastAndFirstLetter(\"%s\") = \"%s\"\n", sTest, swapLastAndFirstLetter(sTest));
+//
+//        sTest = "Testing Running Walking Jumping";
+//        System.out.printf("withoutString(\"%s\",\"%s\") = \"%s\"\n", sTest, "ing", withoutString(sTest, "ing"));
+//        System.out.printf("withoutString(\"%s\",\"%s\") = \"%s\"\n", sTest, "Sleep", withoutString(sTest, "Sleep"));
+//
+//        sTest = "a1b2c3";
+//        System.out.printf("sumNumbersInString(\"%s\") = %d\n", sTest, sumNumbersInString(sTest));
+//        sTest = "aa12bb34cc56dd78";
+//        System.out.printf("sumNumbersInString(\"%s\") = %d\n", sTest, sumNumbersInString(sTest));
+//        sTest = "a9D>Zc8";
+//        System.out.printf("addUnicodeValues(\"%s\") = %d\n", sTest, addUnicodeValues(sTest));
+    }
+}
