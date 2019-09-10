eb3650d1dc2f825ce85177e2e62b44b97fa81b88
Anh Kieu <aqk001@bucknell.edu>
2019-09-10 13:22:06 -0400

    "Lab 5, Ex1 - complete"

---
+++ src/lab05/HelloMain.java
@@ -0,0 +1,39 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2019
+ * Instructor: Prof. Brian King
+ *
+ * Name: YOUR NAME
+ * Section: YOUR SECTION
+ * Date: 9/10/2019
+ * Time: 12:40 PM
+ *
+ * Project: csci205_labs
+ * Package: lab05
+ * Class: HelloMain
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+
+package lab05;
+
+import java.util.Scanner;
+
+/**
+ * A Hello Main class
+ *
+ * @author Anh Kieu - aqk001
+ */
+public class HelloMain {
+    /**
+     * Main method for the program
+     *
+     * @param args Command line arguments that are unused in this program
+     */
+    public static void main(String[] args) {
+        System.out.println("For beautiful eyes, look for the good in others; for beautiful lips, speak only words of kindness; and for poise, walk with the knowledge that you are never alone.");
+        System.out.println("-- Audrey Hepburn");
+    }
+}
